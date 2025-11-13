018acbcaa62052618877a65cd438b9550b8c259a
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-11-13 19:04:42 +0100

    update hero.tsx

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { Marquee } from "@devnomic/marquee";
-import "@devnomic/marquee/dist/index.css";
 
 const Hero = () => {
   return (
